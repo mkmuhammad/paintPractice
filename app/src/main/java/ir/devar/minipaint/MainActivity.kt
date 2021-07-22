bdc94b75d5827abe8494735f63f63cee1f4967d9
devAr <muhammad.keshavarz1377@gmail.com>
2021-07-22 10:42:08 +0430

    add ViewBinding & attrs

--- app/src/main/java/ir/devar/minipaint/MainActivity.kt
+++ app/src/main/java/ir/devar/minipaint/MainActivity.kt
@@ -6,11 +6,16 @@ import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.WindowManager
+import ir.devar.minipaint.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 //        val myCanvasView = MyCanvasView(this)
 //        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
